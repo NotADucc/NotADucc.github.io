1197732197bcbe842dffd62087f68573eec1514d
Guillaume Desktop <robertohelper@outlook.com>
2024-10-20 21:07:34 +0200

    feature: split js from script tag into seperate file, also added noise as  cute little thing

---
+++ public/assets/static/js/noise.js
@@ -0,0 +1,71 @@
+let m = document.getElementById("noise");
+let width = m.width;
+let height = m.height;
+m = m.getContext("2d");
+console.log(width);
+
+draw = (x, y, c, s) => {
+    m.fillStyle = c;
+    m.fillRect(x, y, s, s);
+}
+particles = []
+particle = (x, y, c) => {
+    return { "x": x, "y": y, "vx": 0, "vy": 0, "color": c };
+}
+random = () => Math.random() * 100 + 50;
+create = (number, color) => {
+    group = []
+    for (let i = 0; i < number; i++) {
+        group.push(particle(random(), random(), color));
+        particles.push(group[i]);
+    }
+    return group;
+}
+
+rule = (particles1, particles2, g) => {
+    for (let i = 0; i < particles1.length; i++) {
+        fx = 0;
+        fy = 0;
+
+        for (let j = 0; j < particles2.length; j++) {
+            a = particles1[i];
+            b = particles2[j];
+            dx = a.x - b.x;
+            dy = a.y - b.y;
+            d = Math.sqrt(dx * dx + dy * dy);
+            if (d > 0 && d < 80) {
+                F = g * 1 / d;
+                fx += (F * dx);
+                fy += (F * dy);
+            }
+            
+        }
+            a.vx = (a.vx + fx) * 0.5;
+            a.vy = (a.vy + fy) * 0.5;
+            a.x += a.vx;
+            a.y += a.vy;
+            if (a.x <= 0 || a.x >= width * 0.9) { a.vx *= -1; }
+            if (a.y <= 0 || a.y >= height * 0.9) { a.vy *= -1; }
+    }
+}
+
+blue = create(70, "#3875ea");
+magenta = create(10, "magenta");
+purple = create(100, "#a62161");
+update = () => {
+    rule(blue, blue, -0.32);
+    rule(blue, magenta, -0.17);
+    rule(blue, purple, 0.35);
+    rule(magenta, magenta, -0.1);
+    rule(magenta, blue, -0.34);
+    rule(purple, purple, 0.15);
+    rule(purple, blue, -0.2);
+    rule(purple, magenta, 0.2);
+    m.clearRect(0, 0, 1000, 500);
+    draw(0, 0, "rgb(23 23 23)", 1000);
+    for (let i = 0; i < particles.length; i++) {
+        draw(particles[i].x, particles[i].y, particles[i].color, 5);
+    }
+    requestAnimationFrame(update);
+}
+update();
---
+++ public/assets/static/js/projects.js
@@ -0,0 +1,68 @@
+let projects = 
+[
+    {
+        "title": "Mogcord",
+        "under_title": "A simple messaging paltform",
+        "image": "mogcord.png",
+        "tools": ["Rust", "MongoDB", "REST API", "JWT", "Auth", "AlpineJs", "HTMX", "Tailwind"],
+        "description": "A messaging platform which allows the user to chat with individuals or with a group of people.",
+    },
+    {
+        "title": ".Inside",
+        "under_title": "A visitor registration tool",
+        "image": "inside.png",
+        "tools": [".NET", "ASP.NET Core", "REST API", "WPF", "ADO.NET", "SQL SERVER", "MySQL"],
+        "description": "A business park with a central reception wanted an application to register visitors to the park. This is necessary for fire safety, so that it is always known who is present on the site. Everyone visiting one of the companies must first register at the reception. To make this process as fast as possible, visitors are given the opportunity to register themselves via a touchscreen provided at the entrance. Through the touchscreen, they can enter their own information and the person they have an appointment with. Upon departure, visitors can check out again using the same touchscreen. We ensure that the privacy of visitors is respected in accordance with GDPR regulations. Visitors that came with a car can park at the car park.",
+    },
+    {
+        "title": "Follow-up system",
+        "under_title": "An interactive platform for tests",
+        "image": "web3.png",
+        "tools": ["React", "Node.Js", "REST API", "JWT", "Auth", "Express", "WebSockets", "MySQL", "Tailwind"],
+        "description": "A follow-up system where teachers can monitor or modify the timer for a test, see asked questions, and extra requested time by students in real time. Also allows the teacher to import bulk csv data.",
+    },
+    {
+        "title": "Electronic Receipts",
+        "under_title": "Tool created to manage expenses",
+        "image": "electronic-receipts.png",
+        "tools": ["Winforms", ".NET Framework", "Entity Framework Core", "SQL Server", "SAP Crystal Reports", "OpenXML"],
+        "description": "An internal tool that acts as a bookkeeping tool, it allows users to manage expenses made for customers or generate PDF and Excel reports.",
+    },
+    {
+        "title": "Monkeys",
+        "under_title": "Optimal simulation where monkeys escape a forest",
+        "image": "monkeys.png",
+        "tools": [".NET", "C#", "MongoDB", "Bitmap"],
+        "description": "A simulation where x amount of monkeys are dropped in a xx * yy sized forest with y trees. The goal was to optimize it as much as possible; to accomplish this, chunks and distance algorithms were used.",
+    },
+    {
+        "title": "Restaurant manager",
+        "under_title": "REST API to book and manage reservations",
+        "image": "web4.png",
+        "tools": ["ASP .NET CORE", "XUNIT", "Moq", "REST API", "ADO.NET", "SQL SERVER",],
+        "description": "REST API to view and manage reservations.",
+    },
+];
+
+let proj_container = document.getElementsByClassName("project-container")[0];
+
+projects.forEach(x => {
+    let proj_item = document.createElement('div');
+    proj_item.className = 'project-item';
+    proj_item.innerHTML = `
+        <div>
+            <a href="public/assets/img/${x.image}" target="_blank" rel="noreferrer">
+                <img src="public/assets/img/${x.image}" class="img-fluid" loading="lazy" alt="${x.image}-screenshot">
+            </a>
+            <div class="project-content">
+                <h3 class="project-title">${x.title}</h3>
+                <h4><i>${x.under_title}</i></h4>
+                <p class="tools">${x.tools.join(' | ')}</p>
+                <p class="project-description">
+                    ${x.description}
+                </p>
+            </div>
+        </div>
+    `;
+    proj_container.appendChild(proj_item);
+});

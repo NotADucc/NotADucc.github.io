6143c340c917d5e5d054ea5bb36753190ab518f3
Guillaume Desktop <robertohelper@outlook.com>
2024-10-22 21:48:39 +0200

    feature: webgl

--- public/assets/static/js/noise.js
+++ public/assets/static/js/noise.js
@@ -16,79 +16,152 @@ button.addEventListener("click", (_) => {
     canvas.height = height;
 });
 window.dispatchEvent(new CustomEvent("resize"));
-const ctx = canvas.getContext("2d");
 
-
-const draw = (x, y, c, w, h) => {
-    ctx.fillStyle = c;
-    ctx.fillRect(x, y, w, h);
-}
-
-const particle = (x, y, c, b) => {
-    return { "x": x, "y": y, "vx": 0, "vy": 0, "color": c, "bounds": b };
-}
-
-const random = () => Math.random() *  50;
-
-const create = (amount, color, b) => {
-    const group = [];
-    for (let i = 0; i < amount; i++) {
-        group.push(particle(random(), random(), color, b));
-        particles.push(group[i]);
+const init = async () => {
+    // ~~ INITIALIZE ~~ Make sure we can initialize WebGPU in the browser
+    // https://carmencincotti.com/2022-04-18/drawing-a-webgpu-triangle/#adapter-and-device
+    if (!navigator.gpu) {
+      console.error(
+        "WebGPU cannot be initialized - navigator.gpu not found"
+      );
+      return null;
     }
-    return group;
-}
-
-const rule = (seekers, targets, attraction) => {
-    for (let i = 0; i < seekers.length; i++) {
-        let fx = 0.0;
-        let fy = 0.0;
-        
-        const seeker = seekers[i];
-        for (let j = 0; j < targets.length; j++) {
-            const target = targets[j];
-            if (seeker.bounds || target.bounds) {
-                dx = seeker.x - target.x;
-                dy = seeker.y - target.y;
-            }
-            else {
-                dx = Math.abs(seeker.x - target.x);
-                dy = Math.abs(seeker.y - target.y);
-                if (dx > canvas.width >> 1) dx = canvas.width - dx;
-                if (dy > canvas.height >> 1) dy = canvas.height - dy;
-            }
-            const d = Math.sqrt(dx * dx + dy * dy);
-            if (d == 0 || d > MAX_DISTANCE) continue;
-            const F = (d <= MIN_DISTANCE ? 0.3 : -attraction) / d;
-            fx += F * dx;
-            fy += F * dy;
-        }
-        
-        seeker.vx = (seeker.vx + fx) * FRICTION;
-        seeker.vy = (seeker.vy + fy) * FRICTION;
-        seeker.x = (seeker.x + seeker.vx + canvas.width) % canvas.width;
-        seeker.y = (seeker.y + seeker.vy + canvas.height) % canvas.height;
+    const adapter = await navigator.gpu.requestAdapter();
+    if (!adapter) {
+      console.error("WebGPU cannot be initialized - Adapter not found");
+      return null;
     }
-}
+    const device = await adapter.requestDevice();
+    device.lost.then(() => {
+      console.error("WebGPU cannot be initialized - Device has been lost");
+      return null;
+    });
 
-const update = () => {
-    rule(blue, blue, 0.32);
-    rule(blue, magenta, -0.4);
-    rule(blue, purple, -0.4);
-    rule(magenta, magenta, 0.1);
-    rule(magenta, purple, -0.2);
-    rule(magenta, blue, 0.4);
-    rule(purple, purple, -0.15);
-    rule(purple, blue, 0.4);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < particles.length; i++) {
-        draw(particles[i].x, particles[i].y, particles[i].color, PARTICLE_SIZE, PARTICLE_SIZE);
+    const context = canvas.getContext("webgpu");
+    if (!context) {
+      console.error(
+        "WebGPU cannot be initialized - Canvas does not support WebGPU"
+      );
+      return null;
     }
-    requestAnimationFrame(update);
-}
 
-const particles = [];
-const blue = create(70, "#3875ea", true);
-const magenta = create(15, "magenta", true);
-const purple = create(150, "#a62161", false);
-update();
+    const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+    context.configure({
+      device, 
+      format: presentationFormat,
+      alphaMode: "opaque"
+    });
+
+    const vertices = new Float32Array([
+      -1.0, -1.0, 0, 1, 1, 0, 0, 1, 
+      -0.0, 1.0, 0, 1, 0, 1, 0, 1, 
+      1.0, -1.0, 0, 1, 0, 0, 1, 1, 
+    ]);
+
+    const vertexBuffer = device.createBuffer({
+      size: vertices.byteLength,
+      usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+      mappedAtCreation: true,
+    });
+    new Float32Array(vertexBuffer.getMappedRange()).set(vertices);
+    vertexBuffer.unmap();
+
+    const vertexBuffersDescriptors = [
+      {
+        attributes: [
+          {
+            shaderLocation: 0,
+            offset: 0,
+            format: "float32x4",
+          },
+          {
+            shaderLocation: 1,
+            offset: 16,
+            format: "float32x4",
+          },
+        ],
+        arrayStride: 32,
+        stepMode: "vertex",
+      },
+    ];
+
+    const shaderModule = device.createShaderModule({
+      code: `
+      struct VertexOut {
+          @builtin(position) position : vec4<f32>,
+          @location(0) color : vec4<f32>,
+      };
+      @vertex
+      fn vertex_main(@location(0) position: vec4<f32>,
+                  @location(1) color: vec4<f32>) -> VertexOut
+      {
+          var output : VertexOut;
+          output.position = position;
+          output.color = color;
+          return output;
+      } 
+      @fragment
+      fn fragment_main(fragData: VertexOut) -> @location(0) vec4<f32>
+      {
+          return fragData.color;
+      } 
+  `,
+    });
+
+    const pipeline = device.createRenderPipeline({
+      layout: "auto",
+      vertex: {
+        module: shaderModule,
+        entryPoint: "vertex_main",
+        buffers: vertexBuffersDescriptors,
+      },
+      fragment: {
+        module: shaderModule,
+        entryPoint: "fragment_main",
+        targets: [
+          {
+            format: presentationFormat,
+          },
+        ],
+      },
+      primitive: {
+        topology: "triangle-list",
+      },
+    });
+
+    const renderPassDescriptor = {
+      colorAttachments: [
+        {
+          loadOp: "clear",
+          clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 0.0 }, 
+          storeOp: "store", 
+        },
+      ],
+    };
+
+    function frame() {
+      renderPassDescriptor.colorAttachments[0].view = context
+        .getCurrentTexture()
+        .createView();
+      const commandEncoder = device.createCommandEncoder();
+
+      const passEncoder =
+        commandEncoder.beginRenderPass(renderPassDescriptor);
+
+      passEncoder.setPipeline(pipeline);
+      passEncoder.setVertexBuffer(0, vertexBuffer);
+
+      passEncoder.draw(3);
+
+      passEncoder.end();
+
+      device.queue.submit([commandEncoder.finish()]);
+
+      requestAnimationFrame(frame);
+    }
+
+    requestAnimationFrame(frame);
+  };
+
+  init();

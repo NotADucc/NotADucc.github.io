3dfda1cf2c6a1b0c3c4fe5c192d911b1f3873665
Guillaume Desktop <robertohelper@outlook.com>
2024-10-23 22:00:31 +0200

    feature: implemented quadtree

--- public/assets/static/js/noise.js
+++ public/assets/static/js/noise.js
@@ -1,11 +1,164 @@
+class Point { constructor(x, y) { this.x = x; this.y = y; } }
+
+class Rectangle {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+
+        this.left = x - w / 2;
+        this.right = x + w / 2;
+        this.top = y - h / 2;
+        this.bottom = y + h / 2;
+    }
+
+    contains(point) {
+        return this.left <= point.x && point.x <= this.right && this.top <= point.y && point.y <= this.bottom;
+      }
+    
+    intersects(range) {   
+        return !(this.right < range.left || range.right < this.left || this.bottom < range.top || range.bottom < this.top);
+    }
+}
+
+class QuadTree {
+    constructor(boundary) {
+        this.boundary = boundary;
+        this.capacity = 10;
+        this.particles = [];
+        this.divided = false;
+    }
+
+    insert(particle) {
+        if (!this.boundary.contains(particle.points)) {
+            return false;
+        }
+
+        if (!this.divided && this.particles.length < this.capacity) {
+            this.particles.push(particle);
+            return true;
+        } 
+        else {
+            this.subdivide();
+
+            return this.northeast.insert(particle) 
+                || this.northwest.insert(particle)
+                || this.southeast.insert(particle)
+                || this.southwest.insert(particle);
+        }
+    }
+
+    subdivide() {
+        if (this.divided) return;
+
+        let x = this.boundary.x;
+        let y = this.boundary.y;
+        let w = this.boundary.w / 2;
+        let h = this.boundary.h / 2;
+
+        let ne = new Rectangle(x + w, y - h, w, h);
+        this.northeast = new QuadTree(ne);
+
+        let nw = new Rectangle(x - w, y - h, w, h);
+        this.northwest = new QuadTree(nw);
+
+        let se = new Rectangle(x + w, y + h, w, h);
+        this.southeast = new QuadTree(se);
+
+        let sw = new Rectangle(x - w, y + h, w, h);
+        this.southwest = new QuadTree(sw);
+
+        this.divided = true;
+
+        for (let p of this.particles) this.insert(p);
+        this.particles = [];
+    }
+
+    query(range, found) {
+        if (!this.boundary.intersects(range)) {
+            return found;
+        }
+
+
+        for (let p of this.particles) {
+            if (range.contains(p.points)) {
+                found.push(p);
+            }
+        }
+
+        if (this.divided) {
+            this.northwest.query(range, found);
+            this.northeast.query(range, found);
+            this.southwest.query(range, found);
+            this.southeast.query(range, found);
+        }
+
+        return found;
+    }
+}
+
+const canvas = document.getElementById("noise");
+const ctx = canvas.getContext("2d");
+const button = document.getElementById("expand_button");
+const FRICTION = 0.6; const PARTICLE_SIZE = 5; const MIN_DISTANCE = 5; const MAX_DISTANCE = 30;
+let quadtree = new QuadTree(new Rectangle(canvas.width / 2, canvas.height / 2, canvas.width, canvas.height));
+const particles = [];
+
+const attraction_dct = 
+{
+    "b": { 
+        "b": 0.32, 
+        "m": 0.4, 
+        "p": 0.4 
+    },
+    "m": { 
+        "b": -0.4, 
+        "m": 0.1, 
+        "p": 0 
+    },
+    "p": { 
+        "b": -0.4, 
+        "m": -0.2, 
+        "p": -0.15 
+    },
+};
+
+const random = (size) => Math.random() * size;
+
+const particle = (k, p, c, b) => {
+    return { "key": k, "points": p, "velocity": new Point(0, 0), "color": c, "bounds": b };
+}
+
+const create = (k, amount, color, b) => {
+    for (let i = 0; i < amount; i++) {
+        particles.push(particle(k, new Point(random(canvas.width), random(canvas.height)), color, b));
+    }
+}
+
+const redraw_tree = () => {
+    quadtree = new QuadTree(new Rectangle(canvas.width / 2, canvas.height / 2, canvas.width, canvas.height));
+    for (let i = 0; i  < particles.length; i++) {
+        quadtree.insert(particles[i]);
+    }
+}
+
+const create_particles = () => {
+    create("b", 70, "#3875ea", true);
+    create("m", 15, "magenta", true);
+    create("p", 150, "#a62161", false);
+    redraw_tree();
+}
+
 addEventListener("resize", (_) => {
     canvas.width = window.innerWidth;
     canvas.height = document.getElementsByClassName("fakeHeader")[0].offsetHeight;
-});
 
-const FRICTION = 0.6; const PARTICLE_SIZE = 5; const MIN_DISTANCE = 5; const MAX_DISTANCE = 30;
-const canvas = document.getElementById("noise");
-const button = document.getElementById("expand_button");
+    particles.length = 0;
+    create_particles();
+});
+window.dispatchEvent(new CustomEvent("resize"));
+
 button.addEventListener("click", (_) => {
     const main = document.getElementsByTagName("main")[0];
     const height = canvas.height == 300 ? 73 : 300;
@@ -14,46 +167,31 @@ button.addEventListener("click", (_) => {
     document.getElementById("expand_button_text").classList.toggle("triangle_rotate");
     main.style.marginTop = `${height}px`;
     canvas.height = height;
+
+    particles.length = 0;
+    create_particles();
 });
-window.dispatchEvent(new CustomEvent("resize"));
-const ctx = canvas.getContext("2d");
 
-
-const draw = (x, y, c, w, h) => {
-    ctx.fillStyle = c;
-    ctx.fillRect(x, y, w, h);
-}
-
-const particle = (x, y, c, b) => {
-    return { "x": x, "y": y, "vx": 0, "vy": 0, "color": c, "bounds": b };
-}
-
-const random = () => Math.random() *  50;
-
-const create = (amount, color, b) => {
-    const group = [];
-    for (let i = 0; i < amount; i++) {
-        group.push(particle(random(), random(), color, b));
-        particles.push(group[i]);
-    }
-    return group;
-}
-
-const rule = (seekers, targets, attraction) => {
-    for (let i = 0; i < seekers.length; i++) {
+const calc_next_positions = () => {
+    for (let i = 0; i < particles.length; i++) {
         let fx = 0.0;
         let fy = 0.0;
         
-        const seeker = seekers[i];
+        const seeker = particles[i];
+        const rect = new Rectangle(seeker.points.x, seeker.points.y, seeker.points.x + MAX_DISTANCE, seeker.points.y + MAX_DISTANCE);
+        const targets = quadtree.query(rect, []);
+
         for (let j = 0; j < targets.length; j++) {
             const target = targets[j];
+            const attraction = attraction_dct[seeker.key][target.key];
+            if (attraction == 0) continue;
             if (seeker.bounds || target.bounds) {
-                dx = seeker.x - target.x;
-                dy = seeker.y - target.y;
+                dx = seeker.points.x - target.points.x;
+                dy = seeker.points.y - target.points.y;
             }
             else {
-                dx = Math.abs(seeker.x - target.x);
-                dy = Math.abs(seeker.y - target.y);
+                dx = Math.abs(seeker.points.x - target.points.x);
+                dy = Math.abs(seeker.points.y - target.points.y);
                 if (dx > canvas.width >> 1) dx = canvas.width - dx;
                 if (dy > canvas.height >> 1) dy = canvas.height - dy;
             }
@@ -64,31 +202,26 @@ const rule = (seekers, targets, attraction) => {
             fy += F * dy;
         }
         
-        seeker.vx = (seeker.vx + fx) * FRICTION;
-        seeker.vy = (seeker.vy + fy) * FRICTION;
-        seeker.x = (seeker.x + seeker.vx + canvas.width) % canvas.width;
-        seeker.y = (seeker.y + seeker.vy + canvas.height) % canvas.height;
+        seeker.velocity.x = (seeker.velocity.x + fx) * FRICTION;
+        seeker.velocity.y = (seeker.velocity.y + fy) * FRICTION;
+        seeker.points.x = (seeker.points.x + seeker.velocity.x + canvas.width) % canvas.width;
+        seeker.points.y = (seeker.points.y + seeker.velocity.y + canvas.height) % canvas.height;
     }
 }
 
+const draw = (x, y, c, w, h) => {
+    ctx.fillStyle = c;
+    ctx.fillRect(x, y, w, h);
+}
+
 const update = () => {
-    rule(blue, blue, 0.32);
-    rule(blue, magenta, -0.4);
-    rule(blue, purple, -0.4);
-    rule(magenta, magenta, 0.1);
-    rule(magenta, purple, -0.2);
-    rule(magenta, blue, 0.4);
-    rule(purple, purple, -0.15);
-    rule(purple, blue, 0.4);
+    calc_next_positions();
+    redraw_tree();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < particles.length; i++) {
-        draw(particles[i].x, particles[i].y, particles[i].color, PARTICLE_SIZE, PARTICLE_SIZE);
+        draw(particles[i].points.x, particles[i].points.y, particles[i].color, PARTICLE_SIZE, PARTICLE_SIZE);
     }
     requestAnimationFrame(update);
 }
 
-const particles = [];
-const blue = create(70, "#3875ea", true);
-const magenta = create(15, "magenta", true);
-const purple = create(150, "#a62161", false);
 update();

137e20dfa94e1b5d63803e6ac57bfb46ef2696da
Guillaume Desktop <robertohelper@outlook.com>
2025-06-27 16:04:09 +0200

    feature: implemented pop ups close #3

--- _data/updateProjects.js
+++ _data/updateProjects.js
@@ -11,7 +11,7 @@ const projects =
         "title": ".Inside",
         "under_title": "A visitor registration tool",
         "image": "inside.png",
-        "tools": [".NET", "C#", "ASP.NET Core", "REST API", "WPF", "ADO.NET", "SQL SERVER", "MySQL", "XUnit", "Moq"],
+        "tools": [".NET", "C#", "ASP.NET Core", "REST API", "WPF", "ADO.NET", "SQL Server", "MySQL", "XUnit", "Moq"],
         "description": "A business park with a central reception wanted an application to register visitors to the park. This is necessary for fire safety, so that it is always known who is present on the site. Everyone visiting one of the companies must first register at the reception. To make this process as fast as possible, visitors are given the opportunity to register themselves via a touchscreen provided at the entrance. Through the touchscreen, they can enter their own information and the person they have an appointment with. Upon departure, visitors can check out again using the same touchscreen. We ensure that the privacy of visitors is respected in accordance with GDPR regulations. Visitors that came with a car can park at the car park.",
     },
     {
@@ -26,14 +26,14 @@ const projects =
         "under_title": "Tool created to manage expenses",
         "image": "electronic-receipts.png",
         "tools": [".NET Framework", "C#", "Winforms", "Entity Framework Core", "SQL Server", "SAP Crystal Reports", "OpenXML"],
-        "description": "An internal tool that acts as a bookkeeping tool, it allows users to manage expenses made for customers or generate PDF and Excel reports.",
+        "description": "An internal bookkeeping tool, it allows users to manage expenses made for customers or generate PDFs and Excel reports.",
     },
     {
         "title": "Monkeys",
-        "under_title": "Optimal simulation where monkeys escape a forest",
+        "under_title": "Optimal escape simulation",
         "image": "monkeys.png",
         "tools": [".NET", "C#", "MongoDB", "Bitmap"],
-        "description": "A simulation where x amount of monkeys are dropped in a xx * yy sized forest with y trees. The goal was to optimize it as much as possible; to accomplish this, chunks and distance algorithms were used.",
+        "description": "A simulation where x amount of monkeys are dropped in a xx * yy sized forest with y trees. The goal was to optimize it as much as possible, to accomplish this, chunks and distance algorithms were used.",
     },
     {
         "title": "Mog Site",
@@ -53,7 +53,7 @@ const projects =
         "title": "Restaurant manager",
         "under_title": "REST API to book and manage reservations",
         "image": "web4.png",
-        "tools": ["ASP .NET CORE", "C#", "XUNIT", "Moq", "REST API", "ADO.NET", "SQL SERVER",],
+        "tools": ["ASP .NET CORE", "C#", "XUNIT", "Moq", "REST API", "ADO.NET", "SQL Server",],
         "description": "REST API to view and manage reservations.",
     },
 ];
@@ -65,24 +65,34 @@ const split = template.split('<div class="project-container"></div>')
 const arr = [];
 arr.push(split[0]);
 arr.push('<div class="project-container">');
-projects.forEach(x => {
+projects.forEach((x, idx) => {
     const img = x.image.split('.');
     arr.push(`
-        <div class="project-item">
-            <div>
-                <a href="public/assets/img/${x.image}" target="_blank" rel="noreferrer">
-                    <img src="public/assets/img/${img[0]}.mini.${img[1]}" class="img-fluid" loading="lazy" alt="${x.image}-screenshot">
-                </a>
-                <div class="project-content">
+        <div>
+            <div class="project-item">
+                <div>
+                    <a href="#popup${idx}">
+                        <img src="public/assets/img/${img[0]}.mini.${img[1]}" class="img fluid" loading="lazy" alt="${x.image}-screenshot">
+                    </a>
+                    <div class="project-content">
+                        <h3 class="project-title">${x.title}</h3>
+                        <h4><i>${x.under_title}</i></h4>
+                        <p class="tools">${x.tools.join(' | ')}</p>
+                    </div>
+                </div>
+            </div>
+            <div id="popup${idx}" class="overlay">
+                <div class="popup">
+                    <img src="public/assets/img/${x.image}" class="img img-pop" loading="lazy" alt="${x.image}-screenshot">
                     <h3 class="project-title">${x.title}</h3>
                     <h4><i>${x.under_title}</i></h4>
-                    <p class="tools">${x.tools.join(' | ')}</p>
+                    <a class="close" href="#">&times;</a>
                     <p class="project-description">
                         ${x.description}
                     </p>
                 </div>
             </div>
-        </div> 
+        </div>
     `);
 });
 arr.push('</div>');
